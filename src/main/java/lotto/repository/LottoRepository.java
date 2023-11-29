f9d0d5a792c46ce396764ce0fab6824d55c24a72
김민규 <alsrbtls88@gmail.com>
2023-11-29 13:32:03 +0900

    dir: move to domain

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -3,13 +3,9 @@ package lotto.repository;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import lotto.Lotto;
+import lotto.domain.entity.Lotto;
 
-/**
- entity의 상태를 update하는 메서드는 별도로 존재하지 않는다.
- entity를 update할 때는 entity를 find하여 엔티티 내부의 메서드를 사용해 상태를 변경한다.
- 또한 되도록 entity의 값은 불변 값으로 하여 값을 변경하는 일 자체를 없도록 한다.
- */
+
 public class LottoRepository {
 
     private final List<Lotto> repository = new ArrayList<>();
@@ -26,4 +22,3 @@ public class LottoRepository {
         return Collections.unmodifiableList(repository);
     }
 }
-
