805c131cfc80bf6eb3ae56d29298cbf143212317
김민규 <alsrbtls88@gmail.com>
2023-11-29 18:31:49 +0900

    feat: 당첨 숫자에 대한 결과 산출

--- src/main/java/lotto/domain/LottoBundle.java
+++ src/main/java/lotto/domain/LottoBundle.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
+import lotto.domain.entity.BonusNumber;
 import lotto.domain.entity.Lotto;
 
 public class LottoBundle {
@@ -9,5 +11,33 @@ public class LottoBundle {
 
     public LottoBundle(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public Map<Rank, Integer> calculateRank(final Lotto winningNumbers, final BonusNumber bonusNumber) {
+        Map<Rank, Integer> countBoard = Rank.createCountBoard();
+
+        lottos.forEach(lotto -> {
+                    increaseMatchRankCount(winningNumbers, bonusNumber, lotto, countBoard);
+                }
+        );
+
+        return countBoard;
+    }
+
+    private static void increaseMatchRankCount(
+            Lotto winningNumbers,
+            BonusNumber bonusNumber,
+            Lotto lotto,
+            Map<Rank, Integer> countBoard
+    ) {
+        Rank rank = Rank.from(lotto.calculateMatchCount(winningNumbers), lotto.contains(bonusNumber.getNumber()));
+        countBoard.put(
+                rank,
+                countBoard.get(rank) + 1
+        );
+    }
+
+    public float calculateRevenueRate(final long totalPrize) {
+        return (float) totalPrize / (lottos.size() * 1_000) * 100;
     }
 }
