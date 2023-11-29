219c6ffe44dc17d4292aa81f6daefcca4b9aac00
김민규 <alsrbtls88@gmail.com>
2023-11-29 18:53:31 +0900

    feat: 결과 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,24 @@
 package lotto.view;
 
 import java.util.List;
+import lotto.domain.Rank;
 import lotto.service.dto.LottoDto;
+import lotto.service.dto.Result;
 import lotto.view.constant.Format;
+import lotto.view.constant.Notice;
+import lotto.view.constant.Regex;
 
 public class OutputView {
+
+    private static final OutputView instance = new OutputView();
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        return instance;
+    }
+
     public void printLottos(final List<LottoDto> lottoDtos) {
         System.out.println();
 
@@ -15,6 +29,44 @@ public class OutputView {
                 System.out.println(
                         lottoDto.numbers()
                 )
+        );
+    }
+
+    public void printResult(final Result result) {
+        printResultHead();
+        printCountPerRank(result);
+        printRevenueRate(result.revenueRate());
+    }
+
+    private void printRevenueRate(final float revenueRate) {
+        Format.REVENUE_RATE.print(
+                Regex.formatRounds(revenueRate)
+        );
+    }
+
+    private void printCountPerRank(final Result result) {
+        printCountOfRank(Rank.FIRST, result.countOfFirst());
+        printCountOfRank(Rank.SECOND, result.countOfSecond());
+        printCountOfRank(Rank.THIRD, result.countOfThird());
+        printCountOfRank(Rank.FORTH, result.countOfForth());
+        printCountOfRank(Rank.FIFTY, result.countOfFifty());
+    }
+
+    private static void printCountOfRank(final Rank first, final int result) {
+        Format.COUNT_OF_RANK.print(
+                first.getCount(),
+                Regex.formatCashPrize(
+                        first.getPrize()
+                ),
+                result
+        );
+    }
+
+    private static void printResultHead() {
+        System.out.println();
+        Notice.NOTICE_RESULT.print();
+        System.out.println(
+                Regex.BAR.getRegex()
         );
     }
 }
