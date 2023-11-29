827bc5a74a26b7ac77465945637834d95cc7aaff
김민규 <alsrbtls88@gmail.com>
2023-11-29 18:52:34 +0900

    feat: 통계 알림

--- src/main/java/lotto/view/constant/Notice.java
+++ src/main/java/lotto/view/constant/Notice.java
@@ -5,6 +5,7 @@ public enum Notice {
     NOTICE_PURCHASE_PRICE("구입금액을 입력해 주세요.\n로또 한장은 1,000원이며, 구입금액은 1,000원 단위로 입력할 수 있습니다."),
     NOTICE_WINNING_NUMBERS("당첨 번호를 입력해 주세요. 당첨 번호는 ','로 구분된 1 ~ 45 범위의 숫자 6개입니다."),
     NOTICE_BONUS_NUMBERS("보너스 번호를 입력해 주세요. 보너스 번호는 당첨 번호와 겹치지 않는 1 ~ 45 범위의 숫자 1개입니다."),
+    NOTICE_RESULT("당첨 통계")
 
     ;
 
