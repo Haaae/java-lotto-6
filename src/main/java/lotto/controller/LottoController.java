be23a431f27d74da7f35ee39b284ee37108c19eb
김민규 <alsrbtls88@gmail.com>
2023-11-29 17:22:32 +0900

    feat: 당첨 숫자 생성, 보너스 숫자 생성, 추첨 결과 출력 로직 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lotto.exception.ExceptionHandler;
 import lotto.service.LottoService;
 import lotto.service.dto.LottoDto;
+import lotto.service.dto.Result;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -21,6 +22,23 @@ public class LottoController {
     public void run() {
         ExceptionHandler.handle(this::generateLottosFromUserInput);
         printPurchaseLottos();
+
+        ExceptionHandler.handle(this::createWinningNumbersFromUserInput);
+
+        Long bonusNumberId = ExceptionHandler.handle(this::createBonusNumberFromUserInput);
+
+        Result result = lottoService.getResult(bonusNumberId);
+        outputView.printResult(result);
+    }
+
+    private long createBonusNumberFromUserInput() {
+        int bonusNumber = inputView.readBonusNumber();
+        return lottoService.createBonusNumber(bonusNumber);
+    }
+
+    private void createWinningNumbersFromUserInput() {
+        List<Integer> winningNumbers = inputView.readWinningNumbers();
+        lottoService.createWinningNumbers(winningNumbers);
     }
 
     private void printPurchaseLottos() {
