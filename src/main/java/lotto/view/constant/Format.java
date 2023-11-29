c5230511856076e2ab6f4b4ac12e196e7694e566
김민규 <alsrbtls88@gmail.com>
2023-11-29 18:53:44 +0900

    feat: 등수, 수익률 포멧

--- src/main/java/lotto/view/constant/Format.java
+++ src/main/java/lotto/view/constant/Format.java
@@ -3,6 +3,8 @@ package lotto.view.constant;
 public enum Format {
 
     COUNT_OF_LOTTO("%d개를 구매했습니다."),
+    COUNT_OF_RANK("%d개 일치 (%s원) - %d개"),
+    REVENUE_RATE("총 수익률은 %f입니다."),
     ;
 
     private final String format;
