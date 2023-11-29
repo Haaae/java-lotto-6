a279d9b62b86ccf2e82967bf1013a0f0dce2ac49
김민규 <alsrbtls88@gmail.com>
2023-11-29 21:48:36 +0900

    fix: 포멧 수정

--- src/main/java/lotto/view/constant/Format.java
+++ src/main/java/lotto/view/constant/Format.java
@@ -2,9 +2,10 @@ package lotto.view.constant;
 
 public enum Format {
 
-    COUNT_OF_LOTTO("%d개를 구매했습니다."),
-    COUNT_OF_RANK("%d개 일치 (%s원) - %d개"),
-    REVENUE_RATE("총 수익률은 %f입니다."),
+    COUNT_OF_LOTTO("%d개를 구매했습니다.\n"),
+    COUNT_OF_RANK("%d개 일치 (%s원) - %d개\n"),
+    COUNT_OF_SECOND("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n"),
+    REVENUE_RATE("총 수익률은 %s%%입니다.\n"),
     ;
 
     private final String format;
