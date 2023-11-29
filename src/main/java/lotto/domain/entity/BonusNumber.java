aed51b9affab44c88905f14bc9b2a6b53a3639bd
김민규 <alsrbtls88@gmail.com>
2023-11-29 17:19:28 +0900

    feat: 보너스 숫자 클래스

---
+++ src/main/java/lotto/domain/entity/BonusNumber.java
@@ -0,0 +1,27 @@
+package lotto.domain.entity;
+
+import java.util.List;
+import lotto.exception.ExceptionCode;
+import lotto.utils.validator.Validator;
+
+public class BonusNumber extends Entity {
+
+    private final long id;
+    private final int number;
+
+    public BonusNumber(final int number, final Lotto winningNumbers) {
+        validate(number, winningNumbers.getNumbers(), ExceptionCode.INVALID_BONUS_NUMBER);
+        this.number = number;
+        this.id = getNextId();
+    }
+
+    private void validate(final int number, final List<Integer> numbers, final ExceptionCode e) {
+        Validator.notContains(numbers, number, e);
+        Validator.isValidRange(this.number, 45, 1, e);
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+}
